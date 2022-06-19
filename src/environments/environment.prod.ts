babd54779c2ba3dd2405a347a34bd55bb082ac64
applex90 <alex20bachmann@gmx.de>
2022-06-19 22:33:57 +0200

    fixed bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  firebase: {
-    projectId: 'ring-of-fire-e4f0f',
-    appId: '1:156670142366:web:37030bc1fad5ad6701188f',
-    storageBucket: 'ring-of-fire-e4f0f.appspot.com',
-    apiKey: 'AIzaSyChouCvbNW2PDlT-NeYX1cIeklCvVtywVA',
-    authDomain: 'ring-of-fire-e4f0f.firebaseapp.com',
-    messagingSenderId: '156670142366',
+    firebase: {
+      apiKey: "AIzaSyChouCvbNW2PDlT-NeYX1cIeklCvVtywVA",
+      authDomain: "ring-of-fire-e4f0f.firebaseapp.com",
+      projectId: "ring-of-fire-e4f0f",
+      storageBucket: "ring-of-fire-e4f0f.appspot.com",
+      messagingSenderId: "156670142366",
+      appId: "1:156670142366:web:37030bc1fad5ad6701188f"
   },
   production: true
 };
